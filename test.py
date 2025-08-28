3c54d1c154d9d1c3be43bb98301ad0512953abd7
JannisNe <necker@physik.hu-berlin.de>
2025-08-28 23:09:06 +0200

    nicer test plot

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 
 def test_plot_spectrum():
     fig, ax = plt.subplots()
-    for k in Spectrum.list_available_spectra():
+    for i, k in enumerate(Spectrum.list_available_spectra()):
         s = Spectrum.from_key(k)
-        s.plot(ax, log=True, energy_scaling=2, label=s.journal)
+        c = f"C{i}"
+        s.plot(ax, log=True, energy_scaling=2, label=s.journal, color=c)
         for cl in s.contour_files:
-            s.plot_cl(cl, ax, log=True, energy_scaling=2, alpha=cl / 100)
+            s.plot_cl(cl, ax, log=True, energy_scaling=2, alpha=cl / 100, color=c)
     ax.set_xlabel("Energy (GeV)")
     ax.set_ylabel("Flux")
+    ax.set_yscale("log")
+    ax.set_xscale("log")
     ax.legend()
     fig.savefig("test_plot_spectrum.pdf")
     plt.close()
